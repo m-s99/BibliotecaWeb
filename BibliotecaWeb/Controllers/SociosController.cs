58602d01a978fa172be08a950f884bc5242970ce
Matias Sabino <55105204+m-s99@users.noreply.github.com>
2022-11-24 01:40:42 -0300

    Socios

---
+++ BibliotecaWeb/Controllers/SociosController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BibliotecaWeb.Data;
+using BibliotecaWeb.Models;
+
+namespace BibliotecaWeb.Controllers
+{
+    public class SociosController : Controller
+    {
+        private readonly smartsof_biblioContext _context;
+
+        public SociosController(smartsof_biblioContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Socios
+        public async Task<IActionResult> Index()
+        {
+            var smartsof_biblioContext = _context.Socios.Include(s => s.Usuario);
+            return View(await smartsof_biblioContext.ToListAsync());
+        }
+
+        // GET: Socios/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Socios == null)
+            {
+                return NotFound();
+            }
+
+            var socio = await _context.Socios
+                .Include(s => s.Usuario)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (socio == null)
+            {
+                return NotFound();
+            }
+
+            return View(socio);
+        }
+
+        // GET: Socios/Create
+        public IActionResult Create()
+        {
+            ViewData["UsuarioId"] = new SelectList(_context.Usuarios, "Id", "Id");
+            return View();
+        }
+
+        // POST: Socios/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Apellido,Nombre,Dni,FechaNacimiento,Domicilio,Telefono,Imagen,UsuarioId,FechaHoraEliminacion,Eliminado")] Socio socio)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(socio);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UsuarioId"] = new SelectList(_context.Usuarios, "Id", "Id", socio.UsuarioId);
+            return View(socio);
+        }
+
+        // GET: Socios/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Socios == null)
+            {
+                return NotFound();
+            }
+
+            var socio = await _context.Socios.FindAsync(id);
+            if (socio == null)
+            {
+                return NotFound();
+            }
+            ViewData["UsuarioId"] = new SelectList(_context.Usuarios, "Id", "Id", socio.UsuarioId);
+            return View(socio);
+        }
+
+        // POST: Socios/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Apellido,Nombre,Dni,FechaNacimiento,Domicilio,Telefono,Imagen,UsuarioId,FechaHoraEliminacion,Eliminado")] Socio socio)
+        {
+            if (id != socio.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(socio);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SocioExists(socio.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UsuarioId"] = new SelectList(_context.Usuarios, "Id", "Id", socio.UsuarioId);
+            return View(socio);
+        }
+
+        // GET: Socios/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Socios == null)
+            {
+                return NotFound();
+            }
+
+            var socio = await _context.Socios
+                .Include(s => s.Usuario)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (socio == null)
+            {
+                return NotFound();
+            }
+
+            return View(socio);
+        }
+
+        // POST: Socios/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Socios == null)
+            {
+                return Problem("Entity set 'smartsof_biblioContext.Socios'  is null.");
+            }
+            var socio = await _context.Socios.FindAsync(id);
+            if (socio != null)
+            {
+                _context.Socios.Remove(socio);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SocioExists(int id)
+        {
+          return _context.Socios.Any(e => e.Id == id);
+        }
+    }
+}
